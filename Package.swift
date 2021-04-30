617d59f60ddc0f2ec6de33a85aa28d36b959ce52
Dana Brooks <dana.brooks@atos.net>
2021-04-30 11:05:01 -0400

    Update Package.swift for 0.1.3

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "RealtimeSDK",
-            url: "https://github.com/danabrooks/RealtimeSDK/releases/download/0.1.1/RealtimeSDK-0.1.1.xcframework.zip",
-            checksum: "d8d82a447b8f18b5d93c55ee1c5b392da6da5b98c41de9630b5692ee2c4529c5"
+            url: "https://github.com/danabrooks/RealtimeSDK/releases/download/0.1.3/RealtimeSDK-0.1.3.xcframework.zip",
+            checksum: "98ed57eb1f8400a3d436be3c6900f0fadb4543663e37e8d0752350e900d4a5cd"
         ),
     ]
 )
