c0b98e86244de838d817e1221bcdc9187ae7f7a4
phanikavi <lnphani@unify.com>
2021-05-04 15:48:58 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "RealtimeSDK",
-            url: "https://github.com/danabrooks/RealtimeSDK/releases/download/0.1.97/RealtimeSDK-0.1.97.xcframework.zip",
+            url: "https://github.com/danabrooks/RealtimeSDK/releases/download/0.1.96/RealtimeSDK-0.1.96.xcframework.zip",
             checksum: "47ed4cbcbddf8190a613ea1f9b8233686abeb8051c00a1a915cd46fc1b51ceca"
         ),
     ]
