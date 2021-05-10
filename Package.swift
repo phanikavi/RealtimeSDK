ff0daf76f5649fac96fbb4ad6d017fd528f0410f
phanikavi <lnphani@unify.com>
2021-05-10 15:35:41 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,19 +12,17 @@ let package = Package(
             name: "RealtimeSDK",
             targets: ["RealtimeSDK"]),
     ],
-    dependencies: [],
-    targets: [
-        .binaryTarget(
-            name: "RealtimeSDK",
-            dependencies: [
+    dependencies: [
                 .package(
                     url: "https://github.com/statsel/WebRTC.git",
                     .upToNextMajor(from: "89.0.0")),
                 .package(
                     url: "https://github.com/phanikavi/apollo-ios.git",
                     .upToNextMajor(from: "0.43.2")),
-            ],
-            exclude: [],
+    ],
+    targets: [
+        .binaryTarget(
+            name: "RealtimeSDK",
             url: "https://github.com/phanikavi/RealtimeSDK/releases/download/5.0.9/RealtimeSDK-5.0.8.xcframework.zip",
             checksum: "97fe928908e67fe8762cfeb029b92b3d34201aa08eda86a3c3d3a20f9fa25c9f"    
         )
