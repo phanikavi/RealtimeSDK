e369e378714d9d34590b997054676652c59a9773
phanikavi <lnphani@unify.com>
2021-06-13 14:17:26 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
             url: "https://github.com/danabrooks/RealtimeSDK/releases/download/5.0.14/RealtimeSDK-5.0.14.xcframework.zip",
             checksum: "ba7360b7959846c1e2a0356613b36767a15e1a8d358ce93ae16a8ac612a1abfc",
             linkerSettings: [
-                   .linkedFramework(url: "https://github.com/phanikavi/WebRTC", .upToNextMinor(from: "90.0.0")),
+                   .linkedFramework("WebRTC"),
                    ]
         ),
     ]
