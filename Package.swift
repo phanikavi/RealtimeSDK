d09ade739b12e94e520ceec48c1ae4f657196ad6
phanikavi <lnphani@unify.com>
2021-06-13 13:57:57 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,27 +1,23 @@
 // swift-tools-version:5.3
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
 let package = Package(
     name: "RealtimeSDK",
-    platforms: [
-        .iOS(.v13)
-    ],
+    platforms: [.iOS(.v13)],
     products: [
         .library(
             name: "RealtimeSDK",
             targets: ["RealtimeSDK"]),
     ],
     dependencies: [
-                .package(
-                    url: "https://github.com/statsel/WebRTC.git",
-                    .upToNextMajor(from: "89.0.0")),
-    ],
+                   .package(url: "https://github.com/phanikavi/apollo-ios", .upToNextMinor(from: "0.43.2")),
+                   .package(url: "https://github.com/phanikavi/WebRTC", .upToNextMinor(from: "90.0.0")),
+                   ],
     targets: [
         .binaryTarget(
             name: "RealtimeSDK",
-            url: "https://github.com/phanikavi/RealtimeSDK/releases/download/5.0.9/RealtimeSDK-5.0.8.xcframework.zip",
-            checksum: "97fe928908e67fe8762cfeb029b92b3d34201aa08eda86a3c3d3a20f9fa25c9f"    
-        )
+            url: "https://github.com/danabrooks/RealtimeSDK/releases/download/5.0.14/RealtimeSDK-5.0.14.xcframework.zip",
+            checksum: "ba7360b7959846c1e2a0356613b36767a15e1a8d358ce93ae16a8ac612a1abfc"
+        ),
     ]
 )
